a4d92dcb3724b06d7d1d414eb30a7d3c2bd683ab
unknown <bigjoe1992111@gmail.com>
2019-06-08 14:03:26 +0800

    edit jp_ptt

--- crawler/jp_travel.py
+++ crawler/jp_travel.py
@@ -2,6 +2,7 @@ from lxml import etree
 import requests
 import time
 import json
+import re
 # def api(thisapi):
 #     resp = requests.get(thisapi)
 #     myip = resp.text
@@ -20,39 +21,49 @@ def my_get(url):
     return resp.text
 addr = 'https://www.ptt.cc'
 count = 0
+pat = '時間([a-zA-Z]{3}.[a-zA-Z]{3}..{1,2} \d{2}:\d{2}:\d{2} \d{4})'
 for page in range(1,2):
     url = 'https://www.ptt.cc/bbs/Japan_Travel/index'+str(page)+'.html'
     resp = requests.get(url,headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'})
     resp.encoding='utf-8'
     print('get web page')
+    print('sleep 3 seconds')
     time.sleep(3)
+    print('wake up')
     html = etree.HTML(resp.text)
     element_title = html.xpath("//div[@class='title']/a/text()")
     element_url = html.xpath("//div[@class='title']/a/@href")
     full_url = [addr+u for u in element_url]
     element_author = html.xpath("//div[@class='author']/text()")
-
     # 每夜都是20個內容，四個項目個數一致
     # print('url',len(element_url))
     # print('author',len(element_author))
     # print(element_title)
     # print('title',len(element_title))
-
     #獲取內容
     resp_cnt = [my_get(u) for u in full_url]
     element_content = [etree.HTML(u).xpath("//div[starts-with(@class,'bbs-screen')]")[0].xpath('string(.)') for u in resp_cnt]
     # 每頁的20文章內容寫入，完成後才會進行下一頁
-    with open('./jp_ptt.txt', 'a', encoding='utf-8')as file:
+    with open('./dataset/ptt', 'a', encoding='utf-8')as file:
         for i in range(0,len(element_content)):
             my_dict = {}
             my_dict['article_author'] = element_author[i]
             my_dict['article_content'] = element_content[i]
             my_dict['article_title'] = element_title[i]
-            my_dict['article_date'] = ''
+            date=re.compile(pat, re.S).findall(element_content[i])
+            if len(date)>0:
+                date = date[0]
+            else:
+                date = ''
+            my_dict['article_date'] = date
             print('page {},item {}'.format(str(page),str(i+1)))
             file.write(json.dumps(my_dict,ensure_ascii=False))
             file.write('\n')
+    print('we sleep 3 seconds,and then we move to next step.')
+    time.sleep(3)
+    print('wake up')
     print('finish page')
+
 
 
 
---
+++ crawler/test.py
@@ -0,0 +1,6 @@
+import re
+teststring ='作者: gyoko (gyoko) 看板: Japan_Travel 標題: [遊記] 08夏末JR PASS日本趴趴走（8天夜車的瘋狂之旅XD）\n 時間: Mon Sep  2 22:51:36 2008'
+pat = '時間: ([a-zA-Z]{3}.[a-zA-Z]{3}..{1,2} \d{2}:\d{2}:\d{2} \d{4})'
+result = re.compile(pat,re.S).findall()
+
+print(result)
